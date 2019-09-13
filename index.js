0999b75e81f3bea39bb1e3e4a108cb578cf85a11
hieumaestro <54903623+hieumaestro@users.noreply.github.com>
2019-09-14 00:17:36 +0700

    Create index.js
    Khai báo 2 biến x, y nhận giá trị số bất kì
    Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Bài tập
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+var x =5;
+var y =7;
+// Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+var sum,dec,mul,div;
+sum=x+y;
+dec=x-y;
+mul=x*y;
+div=x/y;
+console.log(sum);
+console.log(dec);
+console.log(mul);
+console.log(div);
